0eb7bc11019ba1a91f888a4555de49c492e99aa8
Skirmantas Kligys <Skirmantas.Kligys@gmail.com>
2024-03-13 09:07:46 -0700

    ddcutil configuration specifies primary, secondary inputs.

--- src/modules/ddcutil.cpp
+++ src/modules/ddcutil.cpp
@@ -7,8 +7,8 @@
 
 namespace {
 
-std::map<uint16_t, std::string> parse_input_names(const Json::Value& config) {
-  std::map<uint16_t, std::string> result;
+std::map<uint8_t, std::string> parse_input_names(const Json::Value& config) {
+  std::map<uint8_t, std::string> result;
   if (config["input-names"].isObject()) {
     const Json::Value& input_names = config["input-names"];
     // Cannot do range-based iteration since no way to access value's key there.
@@ -18,11 +18,20 @@ std::map<uint16_t, std::string> parse_input_names(const Json::Value& config) {
       const std::string key = it.key().asString();
       const std::string value = it->asString();
       const unsigned long key_ul = std::stoul(key, nullptr, 0);
-      if (key_ul > UINT16_MAX) throw std::runtime_error("Input names key out of range");
-      result.emplace(static_cast<uint16_t>(key_ul), value);
+      if (key_ul > UINT8_MAX) throw std::runtime_error("Input names key out of range");
+      result.emplace(static_cast<uint8_t>(key_ul), value);
     }
   }
   return result;
+}
+
+uint8_t lookup_input(const Json::Value& value, const std::map<uint8_t, std::string>& input_names) {
+  if (!value.isString()) throw std::runtime_error("Input is not a string");
+  const std::string value_str = value.asString();
+  for (const auto& [k, v] : input_names) {
+    if (v == value_str) return k;
+  }
+  throw std::runtime_error("Input name not found");
 }
 
 struct DisplayIdDeleter {
@@ -63,12 +72,6 @@ std::unique_ptr<DDCA_Display_Handle, DisplayHandleCloser> open_display(const DDC
   return display_handle;
 }
 
-std::string source_to_class(const std::string& source) {
-  if (source == "DP1") return "primary";
-  else if (source == "DP2") return "secondary";
-  else return "error";
-}
-
 }
 
 waybar::modules::DdcUtil::DdcUtil(const std::string& id, const Bar& bar,
@@ -77,7 +80,9 @@ waybar::modules::DdcUtil::DdcUtil(const std::string& id, const Bar& bar,
       bar_(bar),
       i2c_bus_(config_["bus"].isUInt() ? config_["bus"].asUInt() : -1),
       input_name_(parse_input_names(config)),
-      status_("starting") {
+      primary_input_(lookup_input(config_["primary-input"], input_name_)),
+      secondary_input_(lookup_input(config_["secondary-input"], input_name_)),
+      curr_input_(0), status_("starting") {
   if (i2c_bus_ < 0) {
     throw std::runtime_error("Specify the I2C bus");
   }
@@ -95,15 +100,15 @@ waybar::modules::DdcUtil::~DdcUtil() {
   // TODO if needed
 }
 
-std::string waybar::modules::DdcUtil::input_source_name(uint16_t input) const {
+std::string waybar::modules::DdcUtil::input_source_name(uint8_t input) const {
   const auto it = input_name_.find(input);
   if (it == input_name_.end()) return "???";
   return it->second;
 }
 
-std::string waybar::modules::DdcUtil::get_input_source() const {
+uint8_t waybar::modules::DdcUtil::get_input_source() const {
   ddca_enable_verify(true);
-  const std::string failed = "???";
+  const uint8_t failed = UINT8_MAX;
 
   const auto display_id = display_id_from_busno(i2c_bus_);
   if (!display_id) return failed;
@@ -123,12 +128,12 @@ std::string waybar::modules::DdcUtil::get_input_source() const {
     spdlog::error("ddcutil: Failed to get input source: {}, {}", ddca_rc_name(rc), ddca_rc_desc(rc));
     return failed;
   }
-  return input_source_name(value.sh << 8 | value.sl);
+  return value.sh << 8 | value.sl;
 }
 
-std::string waybar::modules::DdcUtil::set_input_source(uint8_t target_input) const {
+uint8_t waybar::modules::DdcUtil::set_input_source(uint8_t target_input) const {
   ddca_enable_verify(true);
-  const std::string failed = "???";
+  const uint8_t failed = UINT8_MAX;
 
   const auto display_id = display_id_from_busno(i2c_bus_);
   if (!display_id) return failed;
@@ -148,17 +153,24 @@ std::string waybar::modules::DdcUtil::set_input_source(uint8_t target_input) con
     return failed;
   }
   // Since verify is on, DDC library verified switching worked.
-  return input_source_name(target_input);
+  return target_input;
+}
+
+std::string waybar::modules::DdcUtil::source_to_class(const uint8_t input) const {
+  if (input == primary_input_) return "primary";
+  else if (input == secondary_input_) return "secondary";
+  else return "error";
 }
 
 void waybar::modules::DdcUtil::worker() {
   thread_ = [this] {
-    const std::string prev_class = source_to_class(status_);
+    const std::string prev_class = source_to_class(curr_input_);
     {
       std::lock_guard<std::mutex> guard(ddc_mutex_);
-      status_ = get_input_source();
+      curr_input_ = get_input_source();
+      status_ = input_source_name(curr_input_);
     }
-    const std::string curr_class = source_to_class(status_);
+    const std::string curr_class = source_to_class(curr_input_);
     if (prev_class != curr_class) {
       label_.get_style_context()->remove_class(prev_class);
       label_.get_style_context()->add_class(curr_class);
@@ -182,19 +194,21 @@ bool waybar::modules::DdcUtil::handleToggle(GdkEventButton* const& e) {
 
     // Only works to switch an input to itself???
     uint8_t target_input;
-    if (status_ == "DP2") target_input = 0x0F;
-    else if (status_ == "DP1") target_input = 0x10;
+    if (curr_input_ == primary_input_) target_input = primary_input_;
+    else if (curr_input_ == secondary_input_) target_input = secondary_input_;
     else {
       spdlog::error("ddcutil: Unknown current input: {}", status_);
       ALabel::handleToggle(e);
       return true;
     }
 
-    const std::string input_source = set_input_source(target_input);
+    const uint8_t prev_input = curr_input_;
+    curr_input_ = set_input_source(target_input);
+    const std::string input_source = input_source_name(curr_input_);
     if (input_source != "???") {
-      const std::string prev_class = source_to_class(status_);
+      const std::string prev_class = source_to_class(prev_input);
       status_ = input_source;
-      const std::string curr_class = source_to_class(status_);
+      const std::string curr_class = source_to_class(curr_input_);
       if (prev_class != curr_class) {
         label_.get_style_context()->remove_class(prev_class);
         label_.get_style_context()->add_class(curr_class);

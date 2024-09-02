ad77c6a83542d230782fcb9cc54593376c1df30d
Skirmantas Kligys <Skirmantas.Kligys@gmail.com>
2024-09-02 13:54:42 -0700

    Native ddcutil module; shows the current input on bus 7, on left click switches.

---
+++ include/modules/ddcutil.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+#include "ALabel.hpp"
+#include "bar.hpp"
+#include "util/sleeper_thread.hpp"
+
+namespace waybar::modules {
+
+class DdcUtil : public ALabel {
+ public:
+  DdcUtil(const std::string&, const waybar::Bar&, const Json::Value&);
+  ~DdcUtil();
+  void update();
+
+ private:
+  void worker();
+  bool handleToggle(GdkEventButton* const& e);
+
+  const Bar&            bar_;
+  std::string           status_;
+  util::SleeperThread   thread_;
+  std::mutex            ddc_mutex_;
+};
+
+}  // namespace waybar::modules
